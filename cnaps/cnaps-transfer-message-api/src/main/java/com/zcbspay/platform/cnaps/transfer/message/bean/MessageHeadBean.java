80aee69ac821736b0a12faf9d08fa6f01c784df6
paytonggithub <896946099@qq.com>
2017-03-03 15:55:25 +0800

    解决MessageHeadBean.java冲突

--- cnaps/cnaps-transfer-message-api/src/main/java/com/zcbspay/platform/cnaps/transfer/message/bean/MessageHeadBean.java
+++ cnaps/cnaps-transfer-message-api/src/main/java/com/zcbspay/platform/cnaps/transfer/message/bean/MessageHeadBean.java
@@ -4,242 +4,245 @@ import java.io.Serializable;
 
 public class MessageHeadBean implements Serializable {
 
-	/**
-	 * serialVersionUID
-	 */
-	private static final long serialVersionUID = -2886084174291829579L;
-	/**
-	 * 起始标识
-	 */
-	private String beginFlag;
-	/**
-	 * 版本号
-	 */
-	private String versionID;
-	/**
-	 * 报文发起人
-	 */
-	private String origSender;
-	/**
-	 * 发送系统号
-	 */
-	private String origSenderSID;
-	/**
-	 * 报文接收人
-	 */
-	private String origReceiver;
-	/**
-	 * 接收系统号
-	 */
-	private String origReceiverSID;
-	/**
-	 * 报文发起日期
-	 */
-	private String origSendDate;
-	/**
-	 * 报文发起时间
-	 */
-	private String origSendTime;
-	/**
-	 * 格式类型
-	 */
-	private String structType;
-	/**
-	 * 报文类型代码
-	 */
-	private String mesgType;
-	/**
-	 * 通信级标识号
-	 */
-	private String mesgID;
-	/**
-	 * 通信级参考号
-	 */
-	private String mesgRefID;
-	/**
-	 * 报文优先级 1：特急； 2：紧急； 3：普通；
-	 */
-	private String mesgPriority;
-	/**
-		由行内发出：U
-		由NPC发出：D
-	 */
-	private String mesgDirection;
-	/**
-	 * （保留域）
-	 */
-	private String reserve;
-	/**
-	 * 结束标识
-	 */
-	private String endFlag;
-	public String toString(){
-		msgHeadStr.append(beginFlag)
-				.append(versionID)
-				.append(origSender)
-				.append(origSenderSID)
-				.append(origReceiver)
-				.append(origReceiverSID)
-				.append(origSendDate)
-				.append(origSendTime)
-				.append(structType)
-				.append(mesgType)
-				.append(mesgID)
-				.append(mesgRefID)
-				.append(mesgPriority)
-				.append(mesgDirection)
-				.append(reserve)
-				.append(endFlag);
-		return msgHeadStr.toString();
-	}
-	public MessageHeadBean(String headMsg){
-		this.beginFlag = headMsg.substring(0, 3);
-		this.versionID = headMsg.substring(3, 5);
-		this.origSender = headMsg.substring(5,19);
-		this.origSenderSID = headMsg.substring(19, 23);
-		this.origReceiver = headMsg.substring(23, 37);
-		this.origReceiverSID = headMsg.substring(37, 41);
-		this.origSendDate = headMsg.substring(41, 49);
-		this.origSendTime = headMsg.substring(49, 55);
-		this.structType = headMsg.substring(55, 58);
-		this.mesgType = headMsg.substring(58, 78);
-		this.mesgID = headMsg.substring(78, 98);
-		this.mesgRefID = headMsg.substring(98, 118);
-		this.mesgPriority = headMsg.substring(118, 119);
-		this.mesgDirection = headMsg.substring(119, 120);
-		this.reserve = headMsg.substring(120, 129);
-		this.endFlag = headMsg.substring(129, 132);
-	}
-	public MessageHeadBean(){
-	}
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = -2886084174291829579L;
+    /**
+     * 起始标识
+     */
+    private String beginFlag;
+    /**
+     * 版本号
+     */
+    private String versionID;
+    /**
+     * 报文发起人
+     */
+    private String origSender;
+    /**
+     * 发送系统号
+     */
+    private String origSenderSID;
+    /**
+     * 报文接收人
+     */
+    private String origReceiver;
+    /**
+     * 接收系统号
+     */
+    private String origReceiverSID;
+    /**
+     * 报文发起日期
+     */
+    private String origSendDate;
+    /**
+     * 报文发起时间
+     */
+    private String origSendTime;
+    /**
+     * 格式类型
+     */
+    private String structType;
+    /**
+     * 报文类型代码
+     */
+    private String mesgType;
+    /**
+     * 通信级标识号
+     */
+    private String mesgID;
+    /**
+     * 通信级参考号
+     */
+    private String mesgRefID;
+    /**
+     * 报文优先级 1：特急； 2：紧急； 3：普通；
+     */
+    private String mesgPriority;
+    /**
+     * 报文传输方向 由行内发出：U 由NPC发出：D
+     */
+    private String mesgDirection;
+    /**
+     * （保留域）
+     */
+    private String reserve;
+    /**
+     * 结束标识
+     */
+    private String endFlag;
 
-	public String getBeginFlag() {
-		return beginFlag;
-	}
+    public String toString() {
+        StringBuffer msgHeadStr = new StringBuffer();
+        msgHeadStr.append(beginFlag)
+                .append(versionID)
+                .append(origSender)
+                .append(origSenderSID)
+                .append(origReceiver)
+                .append(origReceiverSID)
+                .append(origSendDate)
+                .append(origSendTime)
+                .append(structType)
+                .append(mesgType)
+                .append(mesgID)
+                .append(mesgRefID)
+                .append(mesgPriority)
+                .append(mesgDirection)
+                .append(reserve)
+                .append(endFlag);
+        return msgHeadStr.toString();
+    }
 
-	public void setBeginFlag(String beginFlag) {
-		this.beginFlag = beginFlag;
-	}
+    public MessageHeadBean(String headMsg) {
+        this.beginFlag = headMsg.substring(0, 3);
+        this.versionID = headMsg.substring(3, 5);
+        this.origSender = headMsg.substring(5,19);
+        this.origSenderSID = headMsg.substring(19, 23);
+        this.origReceiver = headMsg.substring(23, 37);
+        this.origReceiverSID = headMsg.substring(37, 41);
+        this.origSendDate = headMsg.substring(41, 49);
+        this.origSendTime = headMsg.substring(49, 55);
+        this.structType = headMsg.substring(55, 58);
+        this.mesgType = headMsg.substring(58, 78);
+        this.mesgID = headMsg.substring(78, 98);
+        this.mesgRefID = headMsg.substring(98, 118);
+        this.mesgPriority = headMsg.substring(118, 119);
+        this.mesgDirection = headMsg.substring(119, 120);
+        this.reserve = headMsg.substring(120, 129);
+        this.endFlag = headMsg.substring(129, 132);
+    }
 
-	public String getVersionID() {
-		return versionID;
-	}
+    public MessageHeadBean() {
 
-	public void setVersionID(String versionID) {
-		this.versionID = versionID;
-	}
+    }
 
-	public String getOrigSender() {
-		return origSender;
-	}
+    public String getBeginFlag() {
+        return beginFlag;
+    }
 
-	public void setOrigSender(String origSender) {
-		this.origSender = origSender;
-	}
+    public void setBeginFlag(String beginFlag) {
+        this.beginFlag = beginFlag;
+    }
 
-	public String getOrigSenderSID() {
-		return origSenderSID;
-	}
+    public String getVersionID() {
+        return versionID;
+    }
 
-	public void setOrigSenderSID(String origSenderSID) {
-		this.origSenderSID = origSenderSID;
-	}
+    public void setVersionID(String versionID) {
+        this.versionID = versionID;
+    }
 
-	public String getOrigReceiver() {
-		return origReceiver;
-	}
+    public String getOrigSender() {
+        return origSender;
+    }
 
-	public void setOrigReceiver(String origReceiver) {
-		this.origReceiver = origReceiver;
-	}
+    public void setOrigSender(String origSender) {
+        this.origSender = origSender;
+    }
 
-	public String getOrigReceiverSID() {
-		return origReceiverSID;
-	}
+    public String getOrigSenderSID() {
+        return origSenderSID;
+    }
 
-	public void setOrigReceiverSID(String origReceiverSID) {
-		this.origReceiverSID = origReceiverSID;
-	}
+    public void setOrigSenderSID(String origSenderSID) {
+        this.origSenderSID = origSenderSID;
+    }
 
-	public String getOrigSendDate() {
-		return origSendDate;
-	}
+    public String getOrigReceiver() {
+        return origReceiver;
+    }
 
-	public void setOrigSendDate(String origSendDate) {
-		this.origSendDate = origSendDate;
-	}
+    public void setOrigReceiver(String origReceiver) {
+        this.origReceiver = origReceiver;
+    }
 
-	public String getOrigSendTime() {
-		return origSendTime;
-	}
+    public String getOrigReceiverSID() {
+        return origReceiverSID;
+    }
 
-	public void setOrigSendTime(String origSendTime) {
-		this.origSendTime = origSendTime;
-	}
+    public void setOrigReceiverSID(String origReceiverSID) {
+        this.origReceiverSID = origReceiverSID;
+    }
 
-	public String getStructType() {
-		return structType;
-	}
+    public String getOrigSendDate() {
+        return origSendDate;
+    }
 
-	public void setStructType(String structType) {
-		this.structType = structType;
-	}
+    public void setOrigSendDate(String origSendDate) {
+        this.origSendDate = origSendDate;
+    }
 
-	public String getMesgType() {
-		return mesgType;
-	}
+    public String getOrigSendTime() {
+        return origSendTime;
+    }
 
-	public void setMesgType(String mesgType) {
-		this.mesgType = mesgType;
-	}
+    public void setOrigSendTime(String origSendTime) {
+        this.origSendTime = origSendTime;
+    }
 
-	public String getMesgID() {
-		return mesgID;
-	}
+    public String getStructType() {
+        return structType;
+    }
 
-	public void setMesgID(String mesgID) {
-		this.mesgID = mesgID;
-	}
+    public void setStructType(String structType) {
+        this.structType = structType;
+    }
 
-	public String getMesgRefID() {
-		return mesgRefID;
-	}
+    public String getMesgType() {
+        return mesgType;
+    }
 
-	public void setMesgRefID(String mesgRefID) {
-		this.mesgRefID = mesgRefID;
-	}
+    public void setMesgType(String mesgType) {
+        this.mesgType = mesgType;
+    }
 
-	public String getMesgPriority() {
-		return mesgPriority;
-	}
+    public String getMesgID() {
+        return mesgID;
+    }
 
-	public void setMesgPriority(String mesgPriority) {
-		this.mesgPriority = mesgPriority;
-	}
+    public void setMesgID(String mesgID) {
+        this.mesgID = mesgID;
+    }
 
-	public String getMesgDirection() {
-		return mesgDirection;
-	}
+    public String getMesgRefID() {
+        return mesgRefID;
+    }
 
-	public void setMesgDirection(String mesgDirection) {
-		this.mesgDirection = mesgDirection;
-	}
+    public void setMesgRefID(String mesgRefID) {
+        this.mesgRefID = mesgRefID;
+    }
 
-	public String getReserve() {
-		return reserve;
-	}
+    public String getMesgPriority() {
+        return mesgPriority;
+    }
 
-	public void setReserve(String reserve) {
-		this.reserve = reserve;
-	}
+    public void setMesgPriority(String mesgPriority) {
+        this.mesgPriority = mesgPriority;
+    }
 
-	public String getEndFlag() {
-		return endFlag;
-	}
+    public String getMesgDirection() {
+        return mesgDirection;
+    }
 
-	public void setEndFlag(String endFlag) {
-		this.endFlag = endFlag;
-	}
-	
-	
+    public void setMesgDirection(String mesgDirection) {
+        this.mesgDirection = mesgDirection;
+    }
+
+    public String getReserve() {
+        return reserve;
+    }
+
+    public void setReserve(String reserve) {
+        this.reserve = reserve;
+    }
+
+    public String getEndFlag() {
+        return endFlag;
+    }
+
+    public void setEndFlag(String endFlag) {
+        this.endFlag = endFlag;
+    }
+
 }
